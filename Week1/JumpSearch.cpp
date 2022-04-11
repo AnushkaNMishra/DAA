682634cf2e5b6c7249f2b858bb887abb8419b723
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2022-04-11 19:34:47 +0530

    Update JumpSearch.cpp

--- Week1/JumpSearch.cpp
+++ Week1/JumpSearch.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void jump(int arr[],int n,int key)
+void jumpsearch(int arr[],int n,int key)
 {
-    int start=0,comp=0,flag=0;
+    int first=0,ele=0,flag=0;
     int end=sqrt(n);
     while(arr[end]<=key && end<n)
-    {   comp++;
-        start=end;
+    {  
+        ele++;
+        first=end;
         end+=sqrt(n);
-        if(end>n-1) end=n;
+        if(end>n-1) 
+            end=n;
     }
-    for(int i=start;i<end;i++)
+    
+    for(int i=first;i<end;i++)
     {
         if(arr[i]==key)
         {
@@ -19,10 +22,13 @@ void jump(int arr[],int n,int key)
           break;
         }
     }
-    if(flag)cout<<"Present "<<comp<<endl;
-    else cout<<"Not present"<<endl;
-   
-}
+    if(flag)
+        cout<<"Present "<<ele<<endl;
+    else 
+        cout<<"Not present "<<<<ele<<endl;
+   }
+
+
 int main()
 {
     int n;
@@ -35,8 +41,7 @@ int main()
         for(int i=0;i<size;i++)cin>>arr[i];
         int key;
         cin>>key;
-        jump(arr,size,key);
+        jumpsearch(arr,size,key);
     }
-
-    return 0;
+ return 0;
 }
